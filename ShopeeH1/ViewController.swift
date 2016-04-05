3abd3ce8ee42dd127124ef62734c18858f9a325c
Jakkawad <sunnysunoc@gmail.com>
2016-04-05 15:03:59 +0700

    Initial Commit

---
+++ ShopeeH1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShopeeH1
+//
+//  Created by Jakkawad Chaiplee on 4/5/2559 BE.
+//  Copyright © 2559 Jakkawad Chaiplee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
